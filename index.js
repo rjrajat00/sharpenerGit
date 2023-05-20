3234c4d5eba8a28fbbf73f02bf25ec8fcd95b072
Rajat Singh <105720162+rjrajat00@users.noreply.github.com>
2023-05-20 20:46:03 +0530

    More Users can be Added

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const appointment = (e) => {
   e.preventDefault();
 
-  const name = document.getElementById("name");
-  const number = document.getElementById("number");
-  const booking = document.getElementById("booking");
+  const name = document.getElementById("name").value;
+  const number = document.getElementById("number").value;
+  const booking = document.getElementById("booking").value;
 
   let data = {
-    name: name.value,
-    number: number.value,
-    booking: booking.value,
+    name: name,
+    number: number,
+    booking: booking,
   };
 
-  localStorage.setItem("data", JSON.stringify(data));
+  let existingData = localStorage.getItem("myData");
+  if (existingData) {
+    existingData = JSON.parse(existingData);
+  } else {
+    existingData = []; // Initialize as an empty array if no data exists
+  }
+
+  // Append the new data to the existing data
+  existingData.push(data);
+
+  localStorage.setItem("myData", JSON.stringify(existingData));
 
   alert("User details submitted successfully!");
 };
