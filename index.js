a3112a08c48551b1e032e817fbe879aa7313a3ca
Rajat Singh <105720162+rjrajat00@users.noreply.github.com>
2023-05-20 22:50:52 +0530

    delete buttom

--- index.js
+++ index.js
@@ -26,6 +26,62 @@ const appointment = (e) => {
   alert("User details submitted successfully!");
 };
 
+function updateDataList(data) {
+  const dataList = document.getElementById("dataList");
+
+  // Clear existing data
+
+  // Add each data set as a list item with a delete button
+  data.forEach(function (item, index) {
+    const listItem = document.createElement("li");
+    listItem.textContent =
+      "Name: " +
+      item.name +
+      ", Contact: " +
+      item.number +
+      " Booking: " +
+      item.booking;
+
+    const deleteButton = document.getElementById("delete");
+
+    deleteButton.addEventListener("click", function () {
+      deleteData(index);
+    });
+
+    listItem.appendChild(deleteButton);
+    dataList.appendChild(listItem);
+  });
+}
+
+// Function to delete data set at the specified index
+function deleteData(index) {
+  // Retrieve existing data from local storage
+  let existingData = localStorage.getItem("myData");
+  if (existingData) {
+    existingData = JSON.parse(existingData);
+  } else {
+    return; // No data to delete
+  }
+
+  // Remove the data set at the specified index
+  existingData.splice(index, 1);
+
+  // Store the updated data in local storage
+  localStorage.setItem("myData", JSON.stringify(existingData));
+
+  // Update the page content
+  updateDataList(existingData);
+}
+
+// Load existing data on page load
+document.addEventListener("DOMContentLoaded", function () {
+  const existingData = localStorage.getItem("myData");
+  if (existingData) {
+    const parsedData = JSON.parse(existingData);
+    updateDataList(parsedData);
+  }
+});
+
 let form = document.getElementById("form");
 
 let btn = document.getElementById("btn");
