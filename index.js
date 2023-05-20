48252d23a71826bddd3b08aa5063baf4aa50b999
Rajat Singh <105720162+rjrajat00@users.noreply.github.com>
2023-05-20 18:30:07 +0530

    Appointment in local storage

--- index.js
+++ index.js
@@ -1,59 +1,20 @@
-console.log("welcome to Delete buttons ");
-
-let form = document.getElementById("addForm");
-
-// Adding an Item
-
-const addItem = (e) => {
+const appointment = (e) => {
   e.preventDefault();
 
-  const newItem = document.getElementById("item").value;
-  const li = document.createElement("li");
-  li.className = "list-group-item";
-  li.appendChild(document.createTextNode(newItem));
+  const name = document.getElementById("name").value;
+  const number = document.getElementById("number").value;
+  const booking = document.getElementById("booking").value;
 
-  const deleteBtn = document.createElement("button");
-  deleteBtn.className = "btn btn-danger btn-sm float-right delete";
-  deleteBtn.appendChild(document.createTextNode("X"));
-  li.appendChild(deleteBtn);
-  let list1 = document.querySelector("ul");
-  list1.appendChild(li);
-};
-form.addEventListener("submit", addItem);
+  let data = {
+    name: name,
+    number: number,
+    booking: booking,
+  };
 
-//Removing An Item
-
-let itemList = document.getElementById("items");
-
-const removeItem = (e) => {
-  if (e.target.classList.contains("delete")) {
-    if (confirm("are you sure")) {
-      const li = e.target.parentElement;
-      itemList.removeChild(li);
-    }
-  }
+  localStorage.setItem("data", JSON.stringify(data));
 };
 
-itemList.addEventListener("click", removeItem);
+let form = document.getElementById("form");
 
-//  Filtering An Item from the search option(2nd search)
-
-const filter = document.getElementById("filter");
-
-const filterItems = (e) => {
-  let text = e.target.value.toLowerCase();
-
-  let items = itemList.getElementsByTagName("li");
-
-  Array.from(items).forEach((item) => {
-    let itemName = item.firstChild.textContent;
-
-    if (itemName.toLowerCase().indexOf(text) != -1) {
-      item.style.display = "block";
-    } else {
-      item.style.display = "none";
-    }
-  });
-};
-
-filter.addEventListener("keyup", filterItems);
+let btn = document.getElementById("btn");
+btn.addEventListener("click", appointment);
