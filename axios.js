d9798675bedbd90d71b08fc78dcda6aca2630698
Rajat Singh <105720162+rjrajat00@users.noreply.github.com>
2023-06-01 23:32:37 +0530

    axios implementation

---
+++ axios.js
@@ -0,0 +1,186 @@
+// GET REQUEST
+function getTodos() {
+  /* axios({
+    method: "get",
+    url: "https://jsonplaceholder.typicode.com/todos",
+  })
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
+    */
+
+  axios
+    .get("https://jsonplaceholder.typicode.com/todos")
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
+}
+
+// POST REQUEST
+function addTodo() {
+  /* axios({
+    method: "post",
+    url: "https://jsonplaceholder.typicode.com/todos",
+    data: {
+      title: "test",
+      body: "puttt",
+    },
+  })
+  */
+  axios
+    .patch("https://jsonplaceholder.typicode.com/todos/1", {
+      data: {
+        title: "updated_post",
+        status: "imcomplete",
+      },
+    })
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
+}
+
+// PUT/PATCH REQUEST
+function updateTodo() {
+  axios
+    .put("https://jsonplaceholder.typicode.com/todos/1", {
+      data: {
+        title: "test2",
+        status: "imcomplete",
+      },
+    })
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
+}
+
+// DELETE REQUEST
+function removeTodo() {
+  axios
+    .delete("https://jsonplaceholder.typicode.com/todos/1")
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
+}
+
+// SIMULTANEOUS DATA
+function getData() {
+  axios
+    .all([
+      axios.get("https://jsonplaceholder.typicode.com/todos/1?_limit=5"),
+      axios.get("https://jsonplaceholder.typicode.com/posts?_limit=5"),
+    ])
+    .then(axios.spread((todos, posts) => showOutput(posts)))
+    .catch((err) => console.error(err));
+}
+
+// CUSTOM HEADERS
+function customHeaders() {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "JW Token",
+    },
+  };
+
+  axios
+    .patch(
+      "https://jsonplaceholder.typicode.com/todos/1",
+      {
+        data: {
+          title: "updated_post",
+          status: "imcomplete",
+        },
+      },
+      config
+    )
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
+}
+
+// TRANSFORMING REQUESTS & RESPONSES
+function transformResponse() {
+  const options = {
+    method: "post",
+    url: "https://jsonplaceholder.typicode.com/todos",
+    data: {
+      title: "Hello India",
+    },
+    transformResponse: axios.defaults.transformResponse.concat((data) => {
+      data.title = data.title.toUpperCase();
+      return data;
+    }),
+  };
+  axios(options).then((res) => showOutput(res));
+}
+
+// ERROR HANDLING
+function errorHandling() {
+  console.log("Error Handling");
+}
+
+// CANCEL TOKEN
+function cancelToken() {
+  console.log("Cancel Token");
+}
+
+// INTERCEPTING REQUESTS & RESPONSES
+
+axios.interceptors.request.use(
+  (config) => {
+    console.log(
+      `${config.method.toUpperCase()} request send to ${
+        config.url
+      } at ${new Date().getTime()}`
+    );
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// AXIOS INSTANCES
+
+// Show output in browser
+function showOutput(res) {
+  document.getElementById("res").innerHTML = `
+    <div class="card card-body mb-4">
+      <h5>Status: ${res.status}</h5>
+    </div>
+  
+    <div class="card mt-3">
+      <div class="card-header">
+        Headers
+      </div>
+      <div class="card-body">
+        <pre>${JSON.stringify(res.headers, null, 2)}</pre>
+      </div>
+    </div>
+  
+    <div class="card mt-3">
+      <div class="card-header">
+        Data
+      </div>
+      <div class="card-body">
+        <pre>${JSON.stringify(res.data, null, 2)}</pre>
+      </div>
+    </div>
+  
+    <div class="card mt-3">
+      <div class="card-header">
+        Config
+      </div>
+      <div class="card-body">
+        <pre>${JSON.stringify(res.config, null, 2)}</pre>
+      </div>
+    </div>
+  `;
+}
+
+// Event listeners
+document.getElementById("get").addEventListener("click", getTodos);
+document.getElementById("post").addEventListener("click", addTodo);
+document.getElementById("update").addEventListener("click", updateTodo);
+document.getElementById("delete").addEventListener("click", removeTodo);
+document.getElementById("sim").addEventListener("click", getData);
+document.getElementById("headers").addEventListener("click", customHeaders);
+document
+  .getElementById("transform")
+  .addEventListener("click", transformResponse);
+document.getElementById("error").addEventListener("click", errorHandling);
+document.getElementById("cancel").addEventListener("click", cancelToken);
