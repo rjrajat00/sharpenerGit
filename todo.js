a42364cd857aa3457e42531da01f1d6e888ff65a
Rajat Singh <105720162+rjrajat00@users.noreply.github.com>
2023-06-04 18:25:42 +0530

    ToDo

---
+++ todo.js
@@ -0,0 +1,75 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+  const taskList = document.getElementById("taskList");
+  const doneList = document.getElementById("doneList");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const taskInput = document.getElementById("task");
+    const descInput = document.getElementById("desc");
+
+    const task = taskInput.value.trim();
+    const desc = descInput.value.trim();
+
+    if (task !== "") {
+      const listItem = createTaskListItem(task, desc);
+      taskList.appendChild(listItem);
+
+      taskInput.value = "";
+      descInput.value = "";
+    }
+  });
+
+  taskList.addEventListener("click", function (event) {
+    if (event.target.classList.contains("check-btn")) {
+      const listItem = event.target.parentNode;
+      const doneItem = createDoneItem(listItem);
+      doneList.appendChild(doneItem);
+      listItem.remove();
+    } else if (event.target.classList.contains("delete-btn")) {
+      const listItem = event.target.parentNode;
+      listItem.remove();
+    }
+  });
+
+  doneList.addEventListener("click", function (event) {
+    if (event.target.classList.contains("delete-btn")) {
+      const listItem = event.target.parentNode;
+      listItem.remove();
+    }
+  });
+
+  function createTaskListItem(task, desc) {
+    const listItem = document.createElement("li");
+    listItem.classList.add("list-group-item");
+
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.classList.add("form-check-input", "check-btn");
+    listItem.appendChild(checkbox);
+
+    const taskText = document.createElement("span");
+    taskText.textContent = task;
+    listItem.appendChild(taskText);
+
+    const descText = document.createElement("p");
+    descText.textContent = desc;
+    listItem.appendChild(descText);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.classList.add("btn", "btn-danger", "delete-btn");
+    listItem.appendChild(deleteBtn);
+
+    return listItem;
+  }
+
+  function createDoneItem(listItem) {
+    const doneItem = listItem.cloneNode(true);
+    const checkbox = doneItem.querySelector(".check-btn");
+    checkbox.remove();
+
+    return doneItem;
+  }
+});
